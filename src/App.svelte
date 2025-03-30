f4ae06b08377095b6ba1f63f16c4f479a7b7e967
Uskhar <uskhardev@gmail.com>
2025-03-30 20:50:01 +0200

    create svelte page

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<main>
+  <div id="content">
+    <h1>Uskhar</h1>
+  </div>
+</main>
+
+<style>
+  main {
+    background: var(--night);
+    color: #fafeeb;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  #content {
+    display: grid;
+    place-items: center;
+    margin: auto;
+    height: 100%;
+  }
+
+  h1 {
+    font-weight: 700;
+  }
+</style>
